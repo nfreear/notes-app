c3f6f761103554afe705c950e784d8516acc911a
Nick Freear <nfreear@yahoo.co.uk>
2022-12-13 00:16:01 +0000

    fix: lib/bookmarklet Javascript

--- docs/lib/bookmarklet.js
+++ docs/lib/bookmarklet.js
@@ -1,12 +1,16 @@
 /**
- * A simple bookmarklet | 30-Oct-2022.
+ * A simple bookmarklet
+ * @author Nick Freear, 30-Oct-2022.
  */
 
-const { location, open, URLSearchParams } = window;
+/* eslint-env browser */
+
+// const { location, open, URLSearchParams } = window;
+
 const title = document.querySelector('title').textContent;
 const url = location.href;
-const TARGET = 'http://localhost:8081/notes-app/save.html?' + new URLSearchParams({ title, url });
-const RES = open(TARGET);
+const TARGET = '%{ORIGIN}/notes-app/save.html?' + new URLSearchParams({ title, url });
+const RES = window.open(TARGET);
 
 console.debug('NN Bookmarklet:', TARGET, RES);
 
